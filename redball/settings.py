07943e2ff20cc4ded04c99b138c9c78d10e7e5fa
harsh-dev82 <darrasingh201@gmail.com>
2025-10-17 21:39:53 +0530

    Updated Player model to fix QR code NoReverseMatch issue and updated views

--- redball/settings.py
+++ redball/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # add here, just after SecurityMiddleware
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -117,6 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # Folder where collected static files will go
+
+# For admin and all other static files
+STATICFILES_DIRS = [BASE_DIR / 'static']  # Optional if you have your own static folder
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
